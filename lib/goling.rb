aa8b5704f2cc44a92a2d85afa2094df38e07f06b
Patrick Hanevold <patrick.hanevold@gmail.com>
2011-10-21 22:07:27 +0200

    cleanup

--- lib/goling.rb
+++ lib/goling.rb
@@ -6,13 +6,20 @@ require 'goling/string'
 require 'goling/proc'
 
 def reduce(regexp,&code)
-  Goling::rules << {
+  rule = regexp.values[0].kind_of?(Hash) ? {
     :match  => regexp.keys[0],
-    :result => regexp.values[0].kind_of?(Hash)                                     ? regexp.values[0][:to]     : regexp.values[0],
-    :lang   => regexp.values[0].kind_of?(Hash)                                     ? regexp.values[0][:lang]   : :ruby, 
-    :inline => regexp.values[0].kind_of?(Hash)&&regexp.values[0].has_key?(:inline) ? regexp.values[0][:inline] : false,
+    :result => regexp.values[0][:to]     || '',
+    :lang   => regexp.values[0][:lang]   || :ruby,
+    :inline => regexp.values[0][:inline] || false,
+    :proc => code
+  } : {
+    :match  => regexp.keys[0],
+    :result => regexp.values[0],
+    :lang   => :ruby,
+    :inline => false,
     :proc => code
   }
+  Goling::rules << rule
 end
 
 module Goling
